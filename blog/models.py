6fd9024cec51099fe50e8838491586e1a8510f15
Oxana Paskida <okcana-1102@mail.ru>
2020-03-26 15:35:14 +0300

    First commit!

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Blog_Project(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    image = models.ImageField(upload_to = 'blog/images/')
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
